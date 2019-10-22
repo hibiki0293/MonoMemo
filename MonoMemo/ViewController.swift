4f952a2494aaf1504c02211f3ee352c79c111be3
hibiki0293 <hibiki893@i.softbank.jp>
2019-10-22 20:40:06 +0900

    Initial Commit

---
+++ MonoMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MonoMemo
+//
+//  Created by 小川響生 on 2019/10/22.
+//  Copyright © 2019 小川響生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
