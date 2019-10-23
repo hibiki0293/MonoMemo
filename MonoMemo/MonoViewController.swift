2f02b58fa1869998625705211f0179bb4fc8f60f
hibiki0293 <hibiki893@i.softbank.jp>
2019-10-23 22:08:12 +0900

    storyboard

---
+++ MonoMemo/MonoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MonoViewController.swift
+//  MonoMemo
+//
+//  Created by 小川響生 on 2019/10/23.
+//  Copyright © 2019 小川響生. All rights reserved.
+//
+
+import UIKit
+
+class MonoViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+}
