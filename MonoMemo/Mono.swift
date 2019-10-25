3dbe8103192d00ce45ccee74a7fd27b763ff6378
hibiki0293 <hibiki893@i.softbank.jp>
2019-10-25 15:06:39 +0900

    makeMonoClass add Action

---
+++ MonoMemo/Mono.swift
@@ -0,0 +1,14 @@
+//
+//  Mono.swift
+//  MonoMemo
+//
+//  Created by 小川響生 on 2019/10/25.
+//  Copyright © 2019 小川響生. All rights reserved.
+//
+
+import Foundation
+class Mono{
+    var name:String = ""
+    var kazu:Int = 0
+    var detail:String = ""
+}
--- MonoMemo/MonoViewController.swift
+++ MonoMemo/MonoViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class MonoViewController: UIViewController {
+    
+    @IBOutlet weak var monoName: UITextField!
+    @IBOutlet weak var monoKazu: UITextField!
+    @IBOutlet weak var monoDetail: UITextField!
+    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    @IBAction func CreateMono(_ sender: UIButton) {
+        let kazu = Int(monoKazu.text!)
+        let mono = Mono()
+        mono.name = monoName.text!
+        mono.kazu = kazu!
+        mono.detail = monoDetail.text!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
