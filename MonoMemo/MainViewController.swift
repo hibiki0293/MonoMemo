03dcaeb56db74591e88bae2378248369b0933fe2
hibiki0293 <hibiki893@i.softbank.jp>
2019-11-08 14:22:55 +0900

    table2

--- MonoMemo/MainViewController.swift
+++ MonoMemo/MainViewController.swift
@@ -12,11 +12,17 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     let defaults = UserDefaults.standard
     @IBOutlet weak var monoTableView: UITableView!
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        let tableData = defaults.array(forKey: "tableData") as! [[String]]
         let sectionData = tableData[section]
         return sectionData.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "monoCell")
+        let tableData = defaults.array(forKey: "tableData") as! [[String]]
+        let sectionData = tableData[(indexPath as NSIndexPath).section]
+        let cellData = sectionData[(indexPath as NSIndexPath).row]
+        cell?.textLabel?.text = cellData
+        return cell!
     }
     func numberOfSections(in tableView: UITableView) -> Int{
         return defaults.array(forKey: "sectionTitle")!.count
@@ -42,13 +48,14 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         // Do any additional setup after loading the view.
         monoTableView.delegate = self
         monoTableView.dataSource = self
-        //全てのsectionが入った配列tableDataを定義
+        //全てのsectionが入った配列tableDataを定義、保存
         var tableData:[[String]] = []
         let sectionTitle = defaults.array(forKey: "sectionTitle") as! [String]
         for title in sectionTitle{
             let section = defaults.array(forKey: title) as! [String]
             tableData.append(section)
         }
+        defaults.set(tableData, forKey: "tableData")
     }
 }
 
