52045b68de68ce283ac71240fa525ea67911262e
hibiki0293 <hibiki893@i.softbank.jp>
2019-11-25 17:12:31 +0900

    add search function

--- MonoMemo/MainViewController.swift
+++ MonoMemo/MainViewController.swift
@@ -8,29 +8,49 @@
 
 import UIKit
 
-class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     var giveData:String = ""
+    var searchActive:Bool = false
+    var resultMonos:[String] = []//検索で使用
     let defaults = UserDefaults.standard
     @IBOutlet weak var monoTableView: UITableView!
+    @IBOutlet weak var monoSearchBar: UISearchBar!
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        if searchActive{
+            return resultMonos.count
+        }else{
         let tableData = defaults.array(forKey: "tableData") as! [[String]]
         let sectionData = tableData[section]
         return sectionData.count
+        }
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "monoCell")
+        if searchActive{
+            cell?.textLabel?.text = resultMonos[indexPath.row]
+        }else{
         let tableData = defaults.array(forKey: "tableData") as! [[String]]
         let sectionData = tableData[(indexPath as NSIndexPath).section]
         let cellData = sectionData[(indexPath as NSIndexPath).row]
         cell?.textLabel?.text = cellData
+        }
         return cell!
     }
     func numberOfSections(in tableView: UITableView) -> Int{
+        if searchActive{
+            return 1
+        }else{
         return defaults.array(forKey: "sectionTitle")?.count ?? 0
+        }
     }
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String?{
+        if searchActive{
+            let searched = "検索結果"
+            return searched
+        }else{
         let sectionTitle = defaults.array(forKey: "sectionTitle") as! [String]
         return sectionTitle[section]
+        }
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let table = defaults.array(forKey: "tableData") as! [[String]]
@@ -58,6 +78,8 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         // Do any additional setup after loading the view.
         monoTableView.delegate = self
         monoTableView.dataSource = self
+        monoSearchBar.delegate = self
+        monoSearchBar.showsCancelButton = true
         //全てのsectionが入った配列tableDataを定義、保存
         if defaults.array(forKey: "sectionTitle") != nil{
         var tableData:[[String]] = []
@@ -81,5 +103,35 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
             detailViewController.selectedMonoName = giveData
            }
        }
+    // 検索機能
+    func searchMonos(searchText:String){
+        if searchText.count != 0{
+            searchActive = true
+            let monos = defaults.array(forKey: "nameList") as! [String]
+            resultMonos = monos.filter{mono in return mono.contains(searchText)}
+            monoTableView.reloadData()
+        }
+    }
+     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+         //検索する
+         searchMonos(searchText: searchText)
+     }
+     //canselボタンが押されると呼ばれる
+     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+
+         monoSearchBar.text = ""
+         self.view.endEditing(true)
+         searchActive = false
+         //tableViewを再読み込みする
+         monoTableView.reloadData()
+     }
+     
+     //Searchボタンが押されると呼ばれる
+     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+         
+         self.view.endEditing(true)
+         //検索する
+         searchMonos(searchText: monoSearchBar.text! as String)
+     }
 }
 
